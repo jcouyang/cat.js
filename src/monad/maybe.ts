dad58c9338f21cc9646777b7dbfb13a6d3374e81
Jichao Ouyang <oyanglulu@gmail.com>
2015-10-14 23:47:37 +0800

    doc in comments

--- src/monad/maybe.ts
+++ src/monad/maybe.ts
@@ -1,4 +1,5 @@
 import I = require("../interfaces");
+
 interface Maybe<A> extends I.Monad<A>, I.Monoid, I.Equivable{
 	mjoin(): Maybe<A>;
 }
@@ -9,21 +10,48 @@ export class Nothing<A> implements Maybe<A>{
 		this.value = undefined
 	}
 	mempty = this;
+	/**
+	 * Monoid append
+	 * ```js
+	 * nothing.mappend(just(3)).eql(nothing)
+	 * ```
+	 */
 	mappend<B>(m: Maybe<B>) {
 		return m;
 	}
 	mjoin() {
 		return this;
 	}
+	/**
+	 * Functor fmap
+	 * 
+	 * ```js
+	 * nothing.fmap(id).eql(nothing);
+	 * ```
+	 */
 	fmap(func) {
 		return this;
 	}
 	mreturn(v) {
 		return new Nothing;
 	}
+	/**
+	 * Monad join
+	 * 
+	 * ```js
+	 * nothing.join().eql(nothing)
+	 * ```
+	 */
 	mbind(func) {
 		return this;
 	}
+	/**
+	 * Applicative apply
+	 * 
+	 * ```js
+	 * nothing.fapply(just(2)).eql(nothing)
+	 * ```
+	 */
 	fapply(m) {
 		return this
 	}
@@ -41,39 +69,61 @@ export class Just<A> implements Maybe<A> {
 	constructor(value: A) {
 		this.value = value;
 	}
-	// Monoid
-	mempty = new Nothing;
+
+	mempty = nothing;
 	/**
-	 * ##Monoid append
+	 * Monoid append
 	 * ```js
-	 * this.fmap(a=>m.fmap(b=>a+b)).mjoin();
+	 * just(2).mappend(just(3)).eql(just(5))
 	 * ```
-	 * @param {Maybe}
-	 * @return {Maybe}
-	 * @api public
 	 */
 	mappend<A>(m: Maybe<A>):Maybe<any> {
 		return this.fmap(a=>m.fmap(b=>a+b)).mjoin();
 	}
-	
-	//Applicative
-	fapply(m) {
-		return m.fmap(_=>this.value(_))
+	/**
+	 * Applicative apply
+	 * 
+	 * ```js
+	 * just(id).fapply(just(2)).eql(just(2))
+	 * ```
+	 */
+	fapply<A extends I.AtoBfunc, B, C>(m:Maybe<C>):Just<B> {
+		return <Just<B>>m.fmap(_=>this.value(_))
 	}
 	pure = Just.of;
 
-	// Functor
-	fmap<B>(func: (A) => A|B): Just<A|B> {
+	/**
+	 * Functor fmap
+	 * 
+	 * ```js
+	 * just(2).fmap(id).eql(just(2));
+	 * ```
+	 */
+	fmap<B>(func: (A) => B): Just<B> {
 		return new Just(func(this.value));
 	}
 	
-	// Monad
+	/**
+	 * Monad join
+	 * 
+	 * ```js
+	 * just(just(3)).mjoin().eql(just(3)).should.be.true
+	 * ```
+	 * `Maybe Maybe a -> Maybe a`
+	 */
 	mjoin():Maybe<A> {
 		return this.mbind(id);
 	}
 	
 	mreturn = this.pure;
-	
+	/**
+	 * Monad bind =<<
+	 * 
+	 * ```js
+	 * just(2).mbind(f).should.eql(f(2));
+	 * ```
+	 * `Maybe a -> (a -> Maybe b) -> Maybe b`
+	 */
 	mbind<B>(func: (A) => Maybe<A>): Maybe<A> {
 		return func(this.value);
 	}
@@ -83,14 +133,25 @@ export class Just<A> implements Maybe<A> {
 	}
 }
 
-// alias of constructor
+/**
+ * alias to Just's constructor
+ */
 export function just(value) { return new Just(value); };
+/**
+ * alias to Nothing()
+ */
 export const nothing = new Nothing();
-
-//maybe :: b -> (a -> b) -> Maybe a -> b 
+/**
+ * maybe function
+ * 
+ * apply function f to Maybe a, if Nothing, return default value
+ * 
+ * `maybe :: b -> (a -> b) -> Maybe a -> b` 
+ * 
+ */
 export function maybe<A, B>(dv: B, f: (A) => B, m: Maybe<A>): B {
 	if (m instanceof Just) {
-		return f(m.mbind(_=> _));
+		return f(m.mjoin());
 	} else {
 		return dv;
 	}
